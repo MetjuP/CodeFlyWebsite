9c10b03a41e897bc355f9b1abc0e4b3b60f48a4c
MetjuP <pawlakmateus@gmail.com>
2020-03-10 18:34:59 +0300

    rozwijanie projektow

--- magic.js
+++ magic.js
@@ -14,7 +14,7 @@ clicked[9]=false;
 const content = new Array(10);
 content[0] = "<h2>Coś o kole</h2><br/><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac consectetur libero, non efficitur risus. Suspendisse vestibulum, lacus eu efficitur porta, mi ipsum cursus eros, ac blandit ante ex molestie nisl. Duis ac egestas est. Cras id sapien vel orci suscipit placerat. Pellentesque convallis, arcu at dictum blandit, lectus tortor lacinia nisl, quis luctus urna sem ultricies risus. Aenean a augue lectus. Mauris accumsan, dui at rhoncus convallis, lorem elit convallis velit, vitae tristique sapien erat elementum sem. Ut sed tempor tellus. Nullam quis convallis nibh, quis iaculis purus.</p>";
 content[1] = '<h2>Kim jesteśmy?</h2><br/><p>Mauris vehicula molestie magna vehicula convallis. Vestibulum sed ipsum sed velit ornare mattis ut eget nisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus est justo, malesuada eget tempus vel, ultrices non quam. Proin vestibulum viverra nisi nec tempor. Morbi ullamcorper mauris eu nulla eleifend condimentum. Donec condimentum a quam in efficitur. Mauris ut sapien aliquet, molestie eros a, mattis ante. Vestibulum vitae consequat augue. Donec mollis vehicula est sed mattis.Mauris ut sapien aliquet, molestie eros a, mattis ante. Vestibulum vitae consequat augue. Donec mollis vehicula est sed mattis.<br/><br/>Sed urna urna, pulvinar id egestas vel, ornare ut ante. Aliquam finibus augue ut mauris facilisis venenatis. Phasellus efficitur laoreet sagittis. Sed sollicitudin est ut dui dictum, eget tempus arcu placerat.  </p>';
-content[2] = '<div class="row"><div class="col-sm-6 offset-6"><h2>Zdać algorytmy</h2></div><div class="col-sm-12 col-xl-6"><div class="mb-3"><img src="img/aktualne1.png"></div><div class="mt-3"><img src="img/aktualne1.png"></div></div><div class="col-sm-12 col-xl-6"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam efficitur orci at quam dictum tempus. Nulla tempus, augue non efficitur euismod, enim tellus egestas mi, et elementum lorem nisi at dui. Praesent laoreet felis sit amet sagittis porttitor. Praesent et nisl vitae felis sollicitudin imperdiet. Nam semper posuere euismod. Donec posuere maximus metus at scelerisque. In non lectus justo. Nulla consequat tincidunt magna, a dictum nisi dignissim vel.</p></div></div>';
+content[2] = '<div class="row"><div class="col-sm-6"><h2>Pójść na browarka</h2></div><div class="w-100"></div><div class="col-sm-12 col-xl-6 mb-3"><p>Morbi euismod non metus vel molestie. Nunc lacus tellus, pellentesque sed dui quis, congue suscipit turpis. Aenean nisi dui, consequat sit amet aliquet non, commodo ac lorem. Morbi porta lacus sit amet tellus imperdiet condimentum. Quisque ut ultricies nisi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis vel scelerisque massa. Pellentesque in ante ac nisi semper gravida. Donec mattis dapibus libero, vitae mattis eros auctor non. Curabitur dignissim sagittis leo, a dapibus augue interdum id. </p></div><div class="col-sm-12 col-xl-6"><div class="mb-3"><img src="img/aktualne2.png"></div><div class="mt-3"><img src="img/aktualne2.png"></div></div></div>';
 content[3] = '';
 content[4] = '';
 content[5] = '';
@@ -25,7 +25,7 @@ content[9] = '';
 const tail =new Array(10);
 tail[0] = 'kafelek2';
 tail[1] = 'kafelek3';
-tail[2] = 'kafelek5';
+tail[2] = 'kafelek6';
 tail[3] = 'kafelek';
 tail[4] = 'kafelek';
 tail[5] = 'kafelek';
@@ -37,7 +37,7 @@ tail[9] = 'kafelek';
 let timer = 0;
 let timer1 = 0;
 let currentNumber = 0;
-let animation = 0;
+let animation = 99;
 
 function hide()
 {
@@ -45,6 +45,8 @@ function hide()
 	if(animation == 0)
 	{
 		timer = setTimeout('changeContent()',500);
+		console.log(currentNumber);
+		console.log(tail[currentNumber]);
 		$('#' + tail[currentNumber]).fadeOut(500);
 	}
 	else if(animation==1)
@@ -59,6 +61,7 @@ function changeContent()
 
 	document.getElementById(tail[currentNumber]).innerHTML = content[currentNumber];
 	document.getElementById(tail[currentNumber]).style.backgroundColor = "transparent";
+	document.getElementById(tail[currentNumber]).style.borderStyle = 'dashed';
 	clicked[currentNumber] = true;
 
 	if(animation==0)
@@ -85,7 +88,7 @@ function showContent(nr)
 }
 function Start()
 {
-	disableScroll();
+	//disableScroll();
 	document.getElementById("wrapper").style.display = 'none';
 	$('#welcomeImg').fadeOut(1);
 	document.getElementById("welcomeImg").innerHTML = '<img  src="img/logo2.png" alt="">';
@@ -107,20 +110,20 @@ function hideWelcome()
 	clearTimeout(timer1);
 	document.getElementById("wrapper").style.display = 'block';
 	$("#welcomeScreen").slideUp(1500);
-	enableScroll();
+	//enableScroll();
 }
-function disableScroll() { 
-    // Get the current page scroll position 
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop; 
-    let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft, 
+// function disableScroll() { 
+//     // Get the current page scroll position 
+//     let scrollTop = window.pageYOffset || document.documentElement.scrollTop; 
+//     let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft, 
   
-        // if any scroll is attempted, set this to the previous value 
-        window.onscroll = function() {  // tutej nie działa
-            window.scrollTo(scrollLeft, scrollTop); 
-        }; 
-} 
+//         // if any scroll is attempted, set this to the previous value 
+//         window.onscroll = function() {  // tutej nie działa
+//             window.scrollTo(scrollLeft, scrollTop); 
+//         }; 
+// } 
   
-function enableScroll() { 
-    window.onscroll = function() {}; 
-} 
+// function enableScroll() { 
+//     window.onscroll = function() {}; 
+// } 
 
